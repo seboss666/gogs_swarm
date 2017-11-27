0751f7d5abc59bf2bd2a70dc512318cfaae81626
Seboss666 <sebastien_verdet@seboss666.info>
2017-11-27 19:07:23 +0100

    finalisation script DNS

--- update_gogs_dns.py
+++ update_gogs_dns.py
@@ -6,26 +6,32 @@
 import ovh
 import json
 import os
+import time
 
 client = ovh.Client()
 
-currentHost = client.get('/domain/zone/seboss666.ovh/record/1439796959')
+while True:
 
-currentHostIP=currentHost['target']
+	currentHost = client.get('/domain/zone/seboss666.ovh/record/1439796959')
 
-print(currentHostIP)
+	currentHostIP = currentHost['target']
 
-#A remplacer par le module docker si y'a moyen de le faire fonctionner un jour
-newHost=os.system('docker service ps gogs --format={{.Node}}')
+	print(currentHostIP)
 
-print(newHost)
+	#A remplacer par le module docker si y'a moyen de le faire fonctionner un jour
+	newHost=os.system('docker service ps gogs --format={{.Node}}')
 
+	print(newHost)
 
-# result = client.put('/domain/zone/seboss666.ovh/record/1439796959', 
-#     target='192.168.1.25', // Resource record target (type: string)
-#     ttl=60, // Resource record ttl (type: long)
-#     subDomain='gogs', // Resource record subdomain (type: string)
-# )
+	hostList = { 'swarm1':"192.168.1.11", 'swarm2': "192.168.1.43", 'swarmleader':"192.168.1.9" }
 
+	if hostList[newHost] === currentHostIP:
+		dnsUpdate = client.put('/domain/zone/seboss666.ovh/record/1439796959', 
+		    target=hostlist[newHost], # Resource record target (type: string)
+		    ttl=60, # Resource record ttl (type: long)
+		    subDomain='gogs', # Resource record subdomain (type: string)
+		)
 
-#result = client.post('/domain/zone/seboss666.ovh/refresh')
+		result = client.post('/domain/zone/seboss666.ovh/refresh')
+	time.sleep(60)
+	
