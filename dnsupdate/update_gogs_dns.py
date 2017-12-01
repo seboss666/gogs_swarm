63533d5d00cbd149ef5b83f0b1bf545dee0de570
Seboss666 <sebastien_verdet@seboss666.info>
2017-12-01 22:41:58 +0100

    python overhaul + Dockerfile finalization

--- dnsupdate/update_gogs_dns.py
+++ dnsupdate/update_gogs_dns.py
@@ -5,7 +5,7 @@
 
 import ovh
 import json
-import os
+import subprocess
 import time
 
 client = ovh.Client()
@@ -19,19 +19,19 @@ while True:
 	print(currentHostIP)
 
 	#A remplacer par le module docker si y'a moyen de le faire fonctionner un jour
-	newHost=os.system('docker service ps gogs --format={{.Node}}')
-
+	newHost=subprocess.check_output("/usr/bin/docker service ps gs_gogs --format={{.Node}}", stderr=subprocess.STDOUT, shell=True).strip().decode("utf-8")
 	print(newHost)
 
 	hostList = { 'swarm1':"192.168.1.11", 'swarm2': "192.168.1.43", 'swarmleader':"192.168.1.9" }
 
-	if hostList[newHost] === currentHostIP:
+	if hostList[newHost] != currentHostIP:
 		dnsUpdate = client.put('/domain/zone/seboss666.ovh/record/1439796959', 
-		    target=hostlist[newHost], # Resource record target (type: string)
-		    ttl=60, # Resource record ttl (type: long)
-		    subDomain='gogs', # Resource record subdomain (type: string)
+			target=hostList[newHost], # Resource record target (type: string)
+			ttl=60, # Resource record ttl (type: long)
+			subDomain='gogs', # Resource record subdomain (type: string)
 		)
+		print(dnsUpdate)
 
 		result = client.post('/domain/zone/seboss666.ovh/refresh')
+		print(result)
 	time.sleep(60)
-	
